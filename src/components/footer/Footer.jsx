792f4c02e25a84a0d94e6936df9e367ef942e6d8
Namish <namishpande2007@gmail.com>
2022-03-01 16:38:00 +0530

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-      Made By <a target="_blank" rel="noreferrer noopener" href="https://github.com/dark-Jedi2108">  Sugxr#2320  </a> and <a target="_blank" rel="noreferrer noopener" href="https://github.com/hardikmirg">HardikMirg#5395</a>  with 💖
+      Made By <a target="_blank" rel="noreferrer noopener" href="https://github.com/dark-Jedi2108">  Tyepo#2320  </a> and <a target="_blank" rel="noreferrer noopener" href="https://github.com/hardikmirg">HardikMirg#5395</a>  with 💖
     </div>
   )
 }
